0327701fdd8bfc77dd22189fb9532647c6b5a29a
Anthony Pasto <anthopasto2006@gmail.com>
2026-09-22 14:29:45 -0400

    ajout footer/header

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ const resultatRoutes = require("./routes/resultat");
 app.use("/", indexRoutes);
 app.use("/", resultatRoutes);
 
+app.use( (req, res) => {
+    res.status(404).render("pages/404");
+});
+
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
